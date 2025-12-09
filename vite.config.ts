819b56bfe31cd0f7b3bdd994bffe8d73f6c32d1d
odoneprodutor <odoneprodutor@gmail.com>
2025-12-09 14:22:55 -0300

    Fix host configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     base: '/yogaflow/', // Deploy to subdirectory
     server: {
       port: 9001,
-      host: 'true',
+      host: true,
     },
     plugins: [react()],
     define: {
